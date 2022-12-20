503a28fa4f7cac45724dd9313e9b8ffdd129d304
Panda-cry <80039775+Panda-cry@users.noreply.github.com>
2022-12-20 22:32:26 +0100

    Load data
    Packed data and loaded into csv

--- csv_parser/parser.py
+++ csv_parser/parser.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import os
 
 
 def impute(df, col_name):
@@ -28,12 +29,64 @@ def get_data(frame_data):
     return frame_data
 
 
-if __name__ == '__main__':
-    data_frame = pd.read_csv(
-        '../Training Data/NYS Weather Data/New York City, NY/New York City, ... 2018-01-01 to 2018-12-31.csv')
-    data = data_frame[
-        ['datetime', 'temp', 'humidity', 'windgust', 'windspeed', 'sealevelpressure', 'cloudcover', 'visibility',
-         'conditions']]
-    data = get_data(data)
+def cover_loads_for_explicit_year(data, directory,year):
+    ind = 0
+    lista = []
 
-    print(data)
+    for list in os.listdir(directory):
+        if year not in list:
+            continue
+
+        for list_csv in os.listdir(directory + '\\' + list):
+            place = directory + '\\' + list + '\\' + list_csv
+            if year not in place:
+                continue
+            data_frame1 = pd.read_csv(directory + '\\' + list + '\\' + list_csv)
+            data_frame1['Time Stamp'] = pd.to_datetime(data_frame1['Time Stamp'])
+            data_frame1['Time Stamp'] = data_frame1['Time Stamp'].dt.strftime('%Y-%m-%dT%H:%M:%S')
+            data_frame1['Time Stamp'] = pd.to_datetime(data_frame1['Time Stamp'],format='%Y-%m-%dT%H:%M:%S')
+
+            data1 = function_to_better_cover(data_frame1)
+
+            temp  = pd.merge(data, data1, on ='Time Stamp')
+            lista.append(temp)
+            ind += 1
+
+    return  pd.concat(lista)
+
+
+def function_to_better_cover(data_frame1):
+    data1 = data_frame1[data_frame1['Name'] == 'N.Y.C.']
+    m = (data1['Time Stamp'].dt.minute == 0) & (data1['Time Stamp'].dt.second == 0)
+    return data1[m]
+
+
+def loop_through_years(years):
+
+    for year in years:
+        path_year = '../Training Data/NYS Weather Data/New York City, NY/New York City, ... {}-01-01 to {}-12-31.csv'.format(year,year)
+        data_frame = pd.read_csv(path_year)
+        data = data_frame[
+            ['datetime', 'temp', 'humidity', 'windgust', 'windspeed', 'sealevelpressure', 'cloudcover', 'visibility',
+             'conditions']]
+        data = get_data(data)
+        data = data.rename(columns={'datetime' : 'Time Stamp'})
+        data['load'] = 0.0
+
+        directory = r"C:\Users\petar\Desktop\ISIS\Training Data\NYS Load  Data"
+        data['Time Stamp'] = pd.to_datetime(data['Time Stamp'], format='%Y-%m-%dT%H:%M:%S')
+
+        result_for_year = cover_loads_for_explicit_year(data,directory,year)
+        result_for_year.to_csv('packed_data_{}.csv'.format(year))
+
+
+if __name__ == '__main__':
+
+    list_years = ['2018','2019','2020','2021']
+    loop_through_years(list_years)
+
+
+
+
+
+
